ce8951e9270a2ebc24e4cb133022bd290e8297c5
Alexander <av_andreev@yahoo.com>
2022-11-28 16:45:45 +1000

    Task8 complete

---
+++ Lesson1/Task8/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите любое положительное число:");
+int N = Convert.ToInt32(Console.ReadLine());
+int k = 1;
+while (k < N+1)
+{
+    if (k % 2 == 0)
+    {
+        Console.Write($"{k} ");
+    }
+    k++;
+}
+Console.WriteLine("");
