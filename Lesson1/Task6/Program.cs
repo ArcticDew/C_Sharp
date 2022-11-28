f1be188451186ab63aa3df9d49e9e05c45a2c9a7
Alexander <av_andreev@yahoo.com>
2022-11-28 16:33:22 +1000

    Task6 complete

---
+++ Lesson1/Task6/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n % 2 == 0)
+{
+    Console.WriteLine($"Число {n} является четным");
+}
+else
+{
+    Console.WriteLine($"Число {n} НЕ является четным");
+}
+Console.WriteLine();
